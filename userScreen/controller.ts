e095c8ff322a47a55dda33e9f968ea3a01c03071
11dotan <122295829+11dotan@users.noreply.github.com>
2023-05-31 22:34:39 +0300

    d

--- userScreen/controller.ts
+++ userScreen/controller.ts
@@ -1,3 +1,5 @@
+////
+
 userMenuCourses.addEventListener("click", () => {
   userCourses(indexEmail);
 });
--- userScreen/dist/controller.js
+++ userScreen/dist/controller.js
@@ -1,3 +1,4 @@
+////
 userMenuCourses.addEventListener("click", function () {
     userCourses(indexEmail);
 });
